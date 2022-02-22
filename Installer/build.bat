7c113a1c403779783950a8068d3aeec81b5a48c2
H0rologium <33498180+H0rologium@users.noreply.github.com>
2022-02-22 03:23:07 -0600

    yue

--- Installer/build.bat
+++
@@ -1,5 +0,0 @@
-@ECHO off
-color A
-pyinstaller --onefile main.spec
-pause
-REM ('bg.png','D:/git/Beyond-the-End-of-the-World-Modpack-/Installer/bg.png', 'DATA'),('default_manifest.json','D:/git/Beyond-the-End-of-the-World-Modpack-/Installer/', 'DATA')
