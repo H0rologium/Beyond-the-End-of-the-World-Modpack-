8281b904e7e31529705c451fc63e1193518bbccd
H0rologium <33498180+H0rologium@users.noreply.github.com>
2022-02-24 04:26:06 -0600

    FIX SCRIPT

--- push new changes.bat
+++ push new changes.bat
@@ -1,8 +1,8 @@
 @ECHO off
 ECHO make commit note now
-set /p bruh =_
+set /p bruh=_
 git add --all
-git commit -m bruh
+git commit -m "%bruh%"
 ECHO ready to push
 pause
 git push https://github.com/H0rologium/Beyond-the-End-of-the-World-Modpack- 1.16.5
